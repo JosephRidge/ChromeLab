4ebf9f3f3b180c3911b82c0b140085c0ba3ecd7e
JosephRidge <josephridge18@gmail.com>
2026-04-10 11:21:21 +0300

    update

--- app/src/main/java/com/jayr/chromelab/ui/screens/authentication/login/login.kt
+++ app/src/main/java/com/jayr/chromelab/ui/screens/authentication/login/login.kt
@@ -7,9 +7,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.Phone
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -17,6 +19,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
@@ -28,8 +32,8 @@ import com.jayr.chromelab.R
 
 @Composable
 fun LoginScreen(modifier: Modifier){
-    //     text input
-    var textInput by remember { mutableStateOf(TextFieldValue("")) }
+//     inputs
+    var emailInput by remember { mutableStateOf(TextFieldValue("")) }
     var passwordInput by remember { mutableStateOf(TextFieldValue("")) }
 
         Column(
@@ -39,9 +43,39 @@ fun LoginScreen(modifier: Modifier){
         ){
             //        lottie animation
             LottieAnimationWidget()
+
             //        email input
-            UserInputWidget()
+            OutlinedTextField(
+                value = emailInput,
+                onValueChange = { emailInput = it },
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Outlined.Email,
+                        contentDescription = "Email"
+                    )
+                },
+                placeholder = {
+                    Text(text="eg. jd@example.com")
+                },
+                maxLines = 1,
+                modifier = Modifier.fillMaxWidth()
+            )
             //        password input
+            OutlinedTextField(
+                value = passwordInput,
+                onValueChange = { passwordInput = it },
+                leadingIcon = {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(R.drawable.password_icon),
+                        contentDescription = "Email"
+                    )
+                },
+                placeholder = {
+                    Text(text="eg. jd@example.com")
+                },
+                maxLines = 1,
+                modifier = Modifier.fillMaxWidth()
+            )
             //        button
         }
 }
@@ -56,24 +90,5 @@ fun LottieAnimationWidget() {
         composition = composition,
         progress = { progress },
         modifier = Modifier.size(350.dp)
-    )
-}
-
-@Composable
-fun UserInputWidget(){
-//     text input
-    var textInput by remember { mutableStateOf(TextFieldValue("")) }
-//    input composable
-    OutlinedTextField(
-        value = textInput,
-        onValueChange = { textInput = it },
-        leadingIcon = {
-            Icon(
-               imageVector = Icons.Outlined.Email,
-               contentDescription = "Email"
-            )
-        },
-        maxLines = 1,
-        modifier = Modifier.fillMaxWidth()
     )
 }
